e3e3b0d198ae039753b4f51f7cc490093a412941
reo-ahagon <reo-ahagon@gr.paramix.co.jp>
2026-05-08 09:38:00 +0900

    Add files via upload

---
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,16 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21  {
+
+	public static void main(String[] args) {
+		Dictionary_Chapter21 dictionary = new Dictionary_Chapter21();
+		
+		dictionary.addWords();
+		
+		String[] targetWords = {"apple", "banana", "grape","orange"};
+		
+		dictionary.search(targetWords);
+
+	}
+
+}
